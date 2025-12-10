b6de799dee7407cfa382fc1260cc4cc74e01f3aa
Diyarka <sikizhanov92@BK.RU>
2025-12-10 14:58:53 +0500

    v0.0.1 initial commit

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello from Node.js server!");
-});
-
-server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
-});
-
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
+// Раздача статических файлов, если нужно
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-    pages.index
-})
+    res.sendFile(path.join(__dirname, "pages", "index.html"));
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
